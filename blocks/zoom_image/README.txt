eb38b5a32fef08752a8bc9f33bef96d81faac255
Remo Laubacher <remo.laubacher@gmail.com>
2013-09-26 08:32:41 +0200

    move from google code to github

---
+++ blocks/zoom_image/README.txt
@@ -0,0 +1,3 @@
+Concrete5 Block by Remo Laubacher 2009,
+Uses the jQuery fancyzoom port by Steve Smith:
+http://orderedlist.com/articles/fancyzoom-meet-jquery
---
+++ controller.php
@@ -0,0 +1,30 @@
+<?php   
+
+defined('C5_EXECUTE') or die(_("Access Denied."));
+
+class ZoomImagePackage extends Package {
+
+	protected $pkgHandle = 'zoom_image';
+	protected $appVersionRequired = '5.2.0';
+	protected $pkgVersion = '1.0.4';
+	
+	public function getPackageDescription() {
+		return t("Adds images and onstates from the library to pages.");
+	}
+	
+	public function getPackageName() {
+		return t("Zoom Image");
+	}
+	
+	public function install() {
+		$pkg = parent::install();
+		
+		// install block		
+		BlockType::installBlockTypeFromPackage('zoom_image', $pkg);
+		
+	}
+
+
+
+
+}
